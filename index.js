9feb25b72f71ed1e591a0e10221217f5170bce02
Ashton Reed <dreamergirl411@gmail.com>
2017-12-02 04:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-function map(array, callback) {
-  const newArray = []
-
-  for (const element of array) {
-    newArray.push(callback(element))
-  }
-  return newArray;
+function lowerCaseDrivers(array){
+  return array.map(function(e){
+    return e.toLowerCase()
+  })
 }
 
-const lowerCaseDrivers = map(drivers, function () {return newArray.toLowerCase()})
-//   const newDrivers = []
+// drivers.map(function(name) {
+//   return lowerCaseDrivers = 
+// })
+
+
+// function map(array, callback) {
+//   const newArray = []
 // 
+//   for (const element of array) {
+//     newArray.push(callback(element))
+//   }
+//   return newArray;
+// }
+// 
+// const lowerCaseDrivers = map(drivers, function () {return newArray.toLowerCase()})
+
+
+//   const newDrivers = []
+//
 //   for (const element of drivers) {
-//     
+//
 //   }
 // }
 
 // function nameToAttributes() {
-//   
+//
 // }
-// 
+//
 // function attributesToPhrase() {
-//   
+//
 // }
