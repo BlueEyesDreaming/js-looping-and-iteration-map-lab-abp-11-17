2efd6e1e0ed4de00ad7ab7a73ba16a16751c8c53
Ashton Reed <dreamergirl411@gmail.com>
2017-12-02 04:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file.
+function map(array, callback) {
+  const newArray = []
+
+  for (const element of array) {
+    newArray.push(callback(element))
+  }
+  return newArray;
+}
+
+const lowerCaseDrivers = map(drivers, function () {return newArray.toLowerCase()})
+//   const newDrivers = []
+// 
+//   for (const element of drivers) {
+//     
+//   }
+// }
+
+// function nameToAttributes() {
+//   
+// }
+// 
+// function attributesToPhrase() {
+//   
+// }
